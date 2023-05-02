67f7d6998e3b176fec1cd713339aa95b639e393e
Pranami Rutvik <123811033+RutvikPranami@users.noreply.github.com>
2023-05-02 12:23:46 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,30 +4,33 @@ const app = express();
 app.use(express.json());
 
  
-//Complete below given Middleware function which check whether number provided in api as params is odd or Even . Example is shown below
 
-//Example: 
-// GET Reqest of API  '/?num=18' --> The router should return {"num":"18","isOdd":false}
 
 function CheckisOdd(req, res, next) {
 
     //Write Your Code here
+ var value = parseInt(req.query.num);
+    if( value%2 == 0 ){
+        req.query.isOdd = false;
+    }else{
+        req.query.isOdd = true;
+    }
+    next();
 
 }
 
 
-app.get('/', CheckisOdd, (req, res) => {
-    
-    //num in data should be replaced by num from the get request route
-    //isOdd in data should be replaced by whether num is odd or even if it odd make it true else false 
-    const data = {
-        "num" : 5,
-        "isOdd": true
-    };
 
+
+
+app.get('/', CheckisOdd, (req, res) => {
+    const data = {
+        "num" : req.query.num,
+        "isOdd" : req.query.isOdd
+    };
     res.send(JSON.stringify(data));
 });
 
 
-module.exports = app;
 
+module.exports = app;
